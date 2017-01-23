a8b530b3168921e02f2046dd9065d674900bd7b9
farb <far.b3yond.driv3n@gmail.com>
2017-01-23 23:57:49 +0100

    Updated: src/memory.c swap_memory(), int_to_str(), memcmp() added

--- src/memory.c
+++ src/memory.c
@@ -1,5 +1,33 @@
 #include <netos.h>
 
+void swap_memory(char *dst, int ndst)
+{
+	int i, j;
+	unsigned char tmp;
+
+	for(i = 0, j = ndst - 1; (i < (ndst / 2) && j > 0); i++, j--) {
+		tmp = dst[j];
+		dst[j] = dst[i];
+		dst[i] = tmp;
+	}
+}
+
+int int_to_str(char *dst, int ndst, int num)
+{
+	int i = 0;
+	unsigned char r;
+
+	for(i = 0; (num > 0 && ndst > 0); i++, ndst--) {
+		r = num % 10;
+		num /= 10;
+		dst[i] = r + 0x30;
+	}
+
+	swap_memory(dst, i);
+
+	return i;
+}
+
 void *memset(void *s, char c, int n)
 {
 	int i;
@@ -9,4 +37,19 @@ void *memset(void *s, char c, int n)
 	}
 
 	return s;
+}
+
+int memcmp(const void *dst, const void *src, int nsrc)
+{
+	int i;
+	int r;
+
+	for(i = 0; i < nsrc; i++) {
+		r = ((char *)dst)[i] - ((char *)src)[i];
+		if(r != 0) {
+			return r;
+		}
+	}
+
+	return 0;
 }
