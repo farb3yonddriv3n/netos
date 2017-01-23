5ddd544e4a083cb775ed124b947739934084f6d2
farb <far.b3yond.driv3n@gmail.com>
2017-01-24 00:02:15 +0100

    Updated: src/include/keyboard.h keystrokes added

--- src/include/keyboard.h
+++ src/include/keyboard.h
@@ -1,7 +1,7 @@
 #ifndef KEYBOARD_H_
 #define KEYBOARD_H_
 
-#define KEY_STROKES		26
+#define KEY_STROKES		28
 #define SC_BREAK		0x80
 
 struct keyboard_map_s {
@@ -11,8 +11,9 @@ struct keyboard_map_s {
 };
 
 enum key_e {
-	KEY_HELD = (1<<1),
-	KEY_UP = (1<<2)
+	KEY_HELD =      (1<<1),
+	KEY_UP =        (1<<2),
+	KEY_PROMPT =    (1<<3),
 };
 
 unsigned char k_keyboard_input(struct netos_s *src);
