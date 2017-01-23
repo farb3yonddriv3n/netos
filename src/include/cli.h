5011491c092496c01af7b0d9b53fe52eb445f97a
farb <far.b3yond.driv3n@gmail.com>
2017-01-23 23:59:35 +0100

    Updated: src/include/cli.h cli commands added

--- src/include/cli.h
+++ src/include/cli.h
@@ -1,6 +1,17 @@
 #ifndef CLI_H_
 #define CLI_H_
 
+#define SZ(m_d) (sizeof(m_d) - 1)
+
+#define CMD_VERSION 		"version"
+#define CMD_VERSION_N		SZ(CMD_VERSION)
+
+#define CMD_HELP 			"help"
+#define CMD_HELP_N			SZ(CMD_HELP)
+
+#define CMD_COMMANDS		"help, version"
+#define CMD_COMMANDS_N		SZ(CMD_COMMANDS)
+
 void k_cli(struct netos_s *src);
 
 #endif
