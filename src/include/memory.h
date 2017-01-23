e54c8c6f7b7858e8fdf77c4848cabe71d2402f48
farb <far.b3yond.driv3n@gmail.com>
2017-01-24 00:02:51 +0100

    Added: src/include/memory.h k_memcmp() added

--- src/include/memory.h
+++ src/include/memory.h
@@ -1,6 +1,11 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
 
+#define k_memcmp(m_src, m_nsrc, m_dst, m_ndst) (m_nsrc == m_ndst && memcmp(m_src, m_dst, m_ndst) == 0)
+
 void *memset(void *s, char c, int n);
+int memcmp(const void *dst, const void *src, int nsrc);
+void swap_memory(char *dst, int ndst);
+int int_to_str(char *dst, int ndst, int num);
 
 #endif
