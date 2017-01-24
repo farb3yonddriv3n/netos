6c786cb7e29f53738b49094717ba193a977b9d33
farb <far.b3yond.driv3n@gmail.com>
2017-01-24 21:44:54 +0100

    Updated: src/kernel.c - intend modified - network_init() added

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,8 @@
+/* =============================================================================
+ * netOS -- 32-bit OS
+ * Copyright (C) 2017 Filip Pancik -- see LICENSE
+ * =============================================================================
+ */
 #include <netos.h>
 
 void kernel_init(void) {
@@ -6,6 +11,8 @@ void kernel_init(void) {
 	memset(&r, 0, sizeof(r));
 
 	k_screen_clear(SCREEN_W, SCREEN_H);
+
+	network_init(&r);
 
 	while(1 == 1) {
 		k_cli(&r);
