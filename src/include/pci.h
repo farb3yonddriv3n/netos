301c92eb3afd4b2fe8f8444eb400a9167309f553
farb <far.b3yond.driv3n@gmail.com>
2017-01-24 21:47:12 +0100

    Updated: src/include/pci.h src/pci.c - moved pci_s - k_pci_write_reg() added - license added

--- src/include/pci.h
+++ src/include/pci.h
@@ -1,9 +1,23 @@
+/* =============================================================================
+ * netOS -- 32-bit OS
+ * Copyright (C) 2017 Filip Pancik -- see LICENSE
+ * =============================================================================
+ */
 #ifndef PCI_H_
 #define PCI_H_
 
 #define PCI_CONFIG_ADDRESS	0x0CF8
 #define PCI_CONFIG_DATA		0x0CFC
+#define PCI_FUNCS_PER_BUS	0x100
+#define PCI_BUS_MAX			0x100
+
+struct pci_s {
+	int bus;
+	int dev;
+	int dv_id;
+};
 
 unsigned int k_pci_read_reg(unsigned char bus, unsigned char device, unsigned char reg);
+void k_pci_write_reg(unsigned char bus, unsigned char device, unsigned char reg, unsigned int data);
 
 #endif
--- src/pci.c
+++ src/pci.c
@@ -1,11 +1,31 @@
+/* =============================================================================
+ * netOS -- 32-bit OS
+ * Copyright (C) 2017 Filip Pancik -- see LICENSE
+ *
+ * http://wiki.osdev.org/PCI
+ * BareMetal-OS/os/drivers/pci.asm
+ * =============================================================================
+ */
 #include <netos.h>
+
+#define ADDRESS (address = (((bus << 16) | (device << 8) | (reg << 2)) & 0x00ffffff) | 0x80000000)
 
 unsigned int k_pci_read_reg(unsigned char bus, unsigned char device, unsigned char reg)
 {
 	unsigned int address;
 
-	address = (((bus << 16) | (device << 8) | (reg << 2)) & 0x00ffffff) | 0x80000000;
+	ADDRESS;
 
 	outdw(PCI_CONFIG_ADDRESS, address);
 	return indw(PCI_CONFIG_DATA);
+}
+
+void k_pci_write_reg(unsigned char bus, unsigned char device, unsigned char reg, unsigned int data)
+{
+	unsigned int address;
+
+	ADDRESS;
+
+	outdw(PCI_CONFIG_ADDRESS, address);
+	outdw(PCI_CONFIG_DATA, data);
 }
