760ddf5d71e813377b6c21821a9358aac6264748
farb <far.b3yond.driv3n@gmail.com>
2017-01-26 23:44:11 +0100

    Updated: include/net.h - arp_s structure modified

--- src/include/net.h
+++ src/include/net.h
@@ -6,11 +6,14 @@
 #ifndef NET_H_
 #define NET_H_
 
+#define MAC_LEN		6
+#define IP4_LEN		4
+
 struct nic_s {
 	const char *name;
 	void (*init)(struct netos_s *os, struct pci_s *p);
 	void (*reset)(struct netos_s *os);
-	void (*transmit)(struct netos_s *os, char *data, int n);
+	void (*transmit)(struct netos_s *os, void *data, unsigned short n);
 	void (*poll)(struct netos_s *os);
 	void (*ack_init)(struct netos_s *os);
 };
@@ -21,18 +24,24 @@ struct nic_map_s {
 };
 
 struct arp_s {
+	unsigned char  	dsthw_s[MAC_LEN]; // Source hardware address - hlen bytes (see above)
+	unsigned char  	srchw_s[MAC_LEN]; // Source hardware address - hlen bytes (see above)
+	unsigned short 	ptype_s;
+
 	unsigned short 	htype; // Hardware type
 	unsigned short 	ptype; // Protocol type
 	unsigned char  	hlen; // Hardware address length (Ethernet = 6)
 	unsigned char  	plen; // Protocol address length (IPv4 = 4)
-	unsigned short 	opcode; // ARP Operation Code
-	unsigned char  	srchw[6]; // Source hardware address - hlen bytes (see above)
-	unsigned char 	srcpr[4]; // Source protocol address - plen bytes (see above). If IPv4 can just be a "u32" type.
-	unsigned char  	dsthw[6]; // Destination hardware address - hlen bytes (see above)
-	unsigned char 	dstpr[4]; // Destination protocol address - plen bytes (see above). If IPv4 can just be a "u32" type.
+	unsigned short  opcode;
+	unsigned char  	srchw[MAC_LEN]; // Source hardware address - hlen bytes (see above)
+	unsigned char 	srcpr[IP4_LEN]; // Source protocol address - plen bytes (see above). If IPv4 can just be a "u32" type.
+	unsigned char  	dsthw[MAC_LEN]; // Destination hardware address - hlen bytes (see above)
+	unsigned char 	dstpr[IP4_LEN]; // Destination protocol address - plen bytes (see above). If IPv4 can just be a "u32" type.
 };
 
 void network_init(struct netos_s *os);
+
+void build_arp(struct netos_s *os, struct arp_s *a);
 
 extern struct nic_s nic_i8254x;
 
