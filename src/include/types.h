80d8e92077b45dcfdf3e457fb42c09e0020d84a1
farb <far.b3yond.driv3n@gmail.com>
2017-01-30 23:20:23 +0100

    Updated: src/include/types.h - struct registers_s added

--- src/include/types.h
+++ src/include/types.h
@@ -8,4 +8,11 @@
 
 #define NULL ((void *)0)
 
+struct registers_s {
+	unsigned int ds;                  // Data segment selector
+	unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
+	unsigned int int_no, err_code;    // Interrupt number and error code (if applicable)
+	unsigned int eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
+};
+
 #endif
