30d82472de307e2e48a5c2f1fab8d7d368a72584
farb <far.b3yond.driv3n@gmail.com>
2017-02-02 23:53:30 +0100

    Updated: src/kernel.c - removed k_pic_mask_clear()

--- src/kernel.c
+++ src/kernel.c
@@ -5,26 +5,6 @@
  */
 #include <netos.h>
 
-#define WW(m_dst, m_src) (*(unsigned int *)m_dst = m_src)
-
-void k_pic_mask_clear(unsigned char irq)
-{
-	unsigned short dx;
-	unsigned char lirq = irq;
-	unsigned short ax;
-
-	if(lirq < 8) {
-		dx = 0x21;
-	} else {
-		dx = 0xA1;
-		lirq -= 8;
-	}
-
-	ax = inb(dx);
-	ax &= ~lirq;
-	outb(dx, ax);
-}
-
 void kernel_init(void)
 {
 	struct netos_s r;
