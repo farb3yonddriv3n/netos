f62d1bba61741d917a20fe28b3fcc1d423e33603
farb <far.b3yond.driv3n@gmail.com>
2017-02-02 23:48:31 +0100

    Updated: src/syscalls/pci.c io_wait() added

--- src/syscalls/pci.c
+++ src/syscalls/pci.c
@@ -29,3 +29,11 @@ void k_pci_write_reg(unsigned char bus, unsigned char device, unsigned char reg,
 	outdw(PCI_CONFIG_ADDRESS, address);
 	outdw(PCI_CONFIG_DATA, data);
 }
+
+void io_wait(void)
+{
+	/* Port 0x80 is used for 'checkpoints' during POST. */
+	/* The Linux kernel seems to think it is free for use :-/ */
+	asm volatile ( "outb %%al, $0x80" : : "a"(0) );
+	/* %%al instead of %0 makes no difference.  TODO: does the register need to be zeroed? */
+}
